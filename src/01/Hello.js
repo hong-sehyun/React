8baaf6ef676e50616a8a4ecfd3e09245aa7e8676
hong-sehyun <sese0733@naver.com>
2023-04-21 10:39:14 +0900

    helloreact

--- src/01/Hello.js
+++ src/01/Hello.js
@@ -1,18 +1,24 @@
+import './Hello.css';
 import logo from '../logo.svg';
+import '../App.css';
 
 const Hello = () => {
+    let n = '세현';
+    let g = 'https://github.com/hong-sehyun';
     return (
         <main className='container'>
             <article data-theme='dark'>
-
-
-                <img src={logo} alt='logo' />
+                <div>
+                    <img src={logo} className='App-logo' alt='logo' />
+                </div>
                 <footer>
                     <div>
-                        Hello React
+                        <hgroup>
+                            <h1>{n}</h1>
+                            <h2><a href={g} > {g}</a></h2>
+                        </hgroup>
                     </div>
                 </footer>
-
             </article>
         </main>
 
