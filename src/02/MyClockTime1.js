49f7dfad9a202071c3fab8a32f7211c6c513fa78
hong-sehyun <sese0733@naver.com>
2023-04-27 09:36:33 +0900

    시계

--- src/02/MyClockTime1.js
+++ src/02/MyClockTime1.js
@@ -3,12 +3,12 @@ import { useState } from "react";
 const MyClockTime1 = () => {
     let t = new Date().toLocaleTimeString();
     const [myTime, setMyTime] = useState(t);
-    let cnt = 0;
-    setTimeout(console.log(++cnt),1000);
-//    let myTime = new Date().toLocaleTimeString();
+   //let cnt = 0;
+ //   setTimeout(console.log(++cnt),1000);
+    setInterval(() => setMyTime(new Date().toLocaleTimeString()), 1000);
     return (
         <footer>
-            <h1><span >{myTime}</span></h1>
+            <h1><span>{t}</span></h1>
         </footer>
     );
 }
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Box from './03/Box';
+//import Box from './03/Box';
 import './App.css';
 //import Hello from './01/Hello';
-//import MyClock from './02/MyClock';
+import MyClock from './02/MyClock';
 //import MyDiv from './04/MyDiv';
 function App() {
   return (
-    <Box />
-//    <MyClock />
+//    <Box />
+    <MyClock />
 //<MyDiv/>
 
   );
