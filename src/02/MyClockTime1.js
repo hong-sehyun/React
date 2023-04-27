f20f8a1c89eb209845bc752a0798dcba4a7a8c04
hong-sehyun <sese0733@naver.com>
2023-04-27 13:17:25 +0900

    시계

--- src/02/MyClockTime1.js
+++ src/02/MyClockTime1.js
@@ -5,7 +5,10 @@ const MyClockTime1 = () => {
     const [myTime, setMyTime] = useState(t);
    //let cnt = 0;
  //   setTimeout(console.log(++cnt),1000);
+
+ //1초에 한번씩 state변수 변경
     setInterval(() => setMyTime(new Date().toLocaleTimeString()), 1000);
+    
     return (
         <footer>
             <h1><span>{t}</span></h1>
