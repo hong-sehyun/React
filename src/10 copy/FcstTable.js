3b514ebf1737145015fb107a51ac2d190eb2062c
hong-sehyun <sese0733@naver.com>
2023-05-25 01:23:21 +0900

    Fcst

--- src/10 copy/FcstTable.js
+++ src/10 copy/FcstTable.js
@@ -16,30 +16,14 @@ const FcstTable = ({datas, gubun}) => {
   //   );
   // }
   
-
-  // const arr = datas.filter((item) => item[0] === dt)
-  // console.log("arr",arr);
-
-  const categ = ['category', 'fcstDate', 'fcstTime', 'fcstValue'];
-//datas = [...new Set(datas)] ;
-  let tags = categ.map((item) =>
-      <tr>
-        <td>{datas[item]}</td>            
-      </tr>
-   );
   
   return (
-    <table>
-      <thead>
         <tr>
           <th scope="col">항목명</th>
           <th scope="col">예측일자</th>
           <th scope="col">예측시간</th>
           <th scope="col">예보 값</th>
-        </tr>        
-          {tags}        
-      </thead>
-    </table>
+        </tr>              
   )
 }
 
